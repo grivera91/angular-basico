04335c8cb51922973c74e2fd41de6ecda461a270
Gianmarco <gianrc91@gmail.com>
2021-03-23 20:08:20 -0500

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import { HeroesModulo } from './heroes/heroe/heroes.module';
+import { ContadorModulo } from './contador/contador.module';
+
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HeroesModulo,
+    ContadorModulo
   ],
   providers: [],
   bootstrap: [AppComponent]
